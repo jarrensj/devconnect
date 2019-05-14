7183fdf51c92397dc486ded35cf566209d4c2b18
jarrensj <sanjose.jarren@gmail.com>
2019-05-14 12:51:32 -0700

    ProfileAbout component

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
 import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 import { Link } from 'react-router-dom';
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
@@ -22,6 +23,7 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
           {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (<Link to='/edit-profile' className='btn btn-dark'>Edit Profile</Link>)}
           <div className='profile-grid my-1'>
             <ProfileTop profile={profile} />
+            <ProfileAbout profile={profile} />
           </div>
         </Fragment>
       )}
