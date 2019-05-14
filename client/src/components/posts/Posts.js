37638e0a0474195a8cad0cec9f8db5b8612b0a45
jarrensj <sanjose.jarren@gmail.com>
2019-05-14 16:54:26 -0700

    Add posts

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,8 +1,9 @@
 import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import Spinner from '../layout/Spinner'
-import PostItem from './PostItem'
+import Spinner from '../layout/Spinner';
+import PostItem from './PostItem';
+import PostForm from './PostForm';
 import { getPosts } from '../../actions/post'
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
@@ -14,7 +15,7 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
     <p className="lead">
       <i className="fas fa-user"></i> Welcome to the community
     </p>
-    {/* PostForm */}
+    <PostForm />
     <div className="posts">
       {posts.map(post => (
         <PostItem key={post._id} post={post} />
